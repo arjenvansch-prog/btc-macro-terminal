0bace2183e18e4aeec88133afbb8c01021da2d2f
arjenvansch-prog <arjenvansch@gmail.com>
2026-05-11 15:03:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_fear_greed():
 
 if isinstance(btc_history.columns, pd.MultiIndex):
     btc_history.columns = btc_history.columns.droplevel(1)
-
+btc_history.columns = btc_history.columns.get_level_values(0)
 btc_history = btc_history.reset_index()
 
 fig = px.line(
