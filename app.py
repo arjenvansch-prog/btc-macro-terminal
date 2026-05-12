b4a12e72708a41ad2b9e3114c30469bec748b2ca
arjenvansch-prog <arjenvansch@gmail.com>
2026-05-12 13:37:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,10 +81,8 @@ def get_fear_greed():
 
     value = int(data["data"][0]["value"])
     label = data["data"][0]["value_classification"]
-
     return value, label
-
-
+fear_value, fear_label = get_fear_greed()
 if isinstance(btc_history.columns, pd.MultiIndex):
     btc_history.columns = btc_history.columns.get_level_values(0)
 
